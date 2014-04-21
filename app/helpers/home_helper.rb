eb51e309f8b968bf38aa5db27393d2b8fe3c09c4
Cloves Carneiro <ccarneiroj@gmail.com>
2014-04-21 18:01:11 -0400

    Added 'nearby' section, and started using description

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,8 @@ module HomeHelper
   def top_deals(limit=3)
     items = RemoteInventory.find_by_city_id(current_city_id, limit: limit)
   end
+
+  def nearby_deals(limit=3)
+    items = RemoteInventory.find_near_city_id(current_city_id, limit: limit)
+  end
 end
--- app/models/remote_inventory.rb
+++ app/models/remote_inventory.rb
@@ -1,6 +1,7 @@
 class RemoteInventory
   class << self
     attr_accessor :host, :api_version, :max_concurrency
+    RemoteInventory.host = 'http://inventory-service-development.herokuapp.com'
 
     def max_concurrency
       @max_concurrency ||= 1
@@ -11,10 +12,15 @@ class RemoteInventory
     end
 
     def find_by_city_id(city_id, limit: 3)
-      RemoteInventory.host = 'http://inventory-service-development.herokuapp.com'
-
       raise "RemoteInventory.host needs to be set" unless host
       JSON.parse(Typhoeus.get("#{host}/api/v#{api_version}/inventory_items/in_city/#{city_id}.json?limit=#{limit}").body).map do |item|
+        Hashie::Mash.new(item)
+      end
+    end
+
+    def find_near_city_id(city_id, limit: 3)
+      raise "RemoteInventory.host needs to be set" unless host
+      JSON.parse(Typhoeus.get("#{host}/api/v#{api_version}/inventory_items/near_city/#{city_id}.json?limit=#{limit}").body).map do |item|
         Hashie::Mash.new(item)
       end
     end
